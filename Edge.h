e343b0ec6f58202b17f0426579b760a0c7148058
janioxavier <cloud_8945@hotmail.com>
2015-11-28 00:12:59 -0300

    Adição do campo source

--- Edge.h
+++ Edge.h
@@ -1,11 +1,13 @@
 #pragma once
 
-template <typename T, typename W>
+template <typename S, typename T, typename W>
 struct Edge {
   Edge() {};
   Edge(T sink) : sink_(sink), weight_(NULL) {};
   Edge(T sink, W weight) : sink_(sink), weight_(weight) {};
-
+  Edge(S source, T sink, W weight) : source_(source), sink_(sink), weight_(weight) {};
+  
+  const S source_;	 
   const T sink_;
   const W weight_;
 };
