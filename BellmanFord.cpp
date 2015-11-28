618f957c35df918ca9ebd2bf9486ddebb8a0f664
janioxavier <cloud_8945@hotmail.com>
2015-11-28 12:37:18 -0300

    Implementação de BellmanFord normal. Não trata ciclos negativos.

---
+++ BellmanFord.cpp
@@ -0,0 +1,69 @@
+#include "BellmanFord.h"
+
+BellmanFord::BellmanFord() {
+}
+
+
+bool BellmanFord::bellmanFord(std::list<Edge<int,int,int> > edgeList,
+			      std::list<int> vertexList, int source) {
+  init(vertexList, source);
+  relaxEdges(edgeList, vertexList.size());
+  return hasNegativeCycle(edgeList);
+}
+
+std::vector<int> BellmanFord::getDistance_() const {
+  return distance_;
+}
+
+std::vector<int> BellmanFord::getPredecessor_() const {
+  return predecessor_;
+}
+
+void BellmanFord::init(std::list<int> vertexList, int source) {
+  for (int i = 0; i < vertexList.size(); i++) {
+    distance_.push_back(INF);
+    predecessor_.push_back(null);
+  }
+  distance_[source] = 0;
+}
+
+void BellmanFord::relaxEdges(std::list<Edge<int,int,int> > edgeList, int size) {
+  for (int i = 1; i < size; i++) {
+    for (std::list<Edge<int,int,int> >::iterator it = edgeList.begin();
+	 it != edgeList.end(); ++it) {
+      Edge<int,int,int> e = *it;
+      if ((distance_[e.source_] + e.weight_) < distance_[e.sink_]) {
+	distance_[e.sink_] = (distance_[e.source_] + e.weight_);
+	predecessor_[e.sink_] = e.source_;
+      }
+    }
+  }
+}
+
+bool BellmanFord::hasNegativeCycle(std::list<Edge<int,int,int> > edgeList) {
+  for (std::list<Edge<int,int,int> >::iterator it = edgeList.begin();
+       it != edgeList.end(); ++it) {
+    Edge<int,int,int> e = *it;
+    if ((distance_[e.source_] + e.weight_) < distance_[e.sink_])
+      return true;
+  }
+  return false;
+}
+
+std::string BellmanFord::toString() {
+  std::stringstream ss;
+  ss << "Vector Distance\n";
+  for (int i = 0; i < distance_.size(); i++) {
+    ss << "[" << i << "]" << " = " << distance_[i] << "\n";
+  }
+  ss << "Vector Predecessor\n";
+  for (int i = 0; i < predecessor_.size(); i++) {
+    ss << "[" << i << "]" << " = " << predecessor_[i] << "\n";
+  }
+  return ss.str();
+}
+
+
+BellmanFord::~BellmanFord() {
+}
+
