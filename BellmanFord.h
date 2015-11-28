d3a647ed676c394c5ee852b8b7f032f57fa33edc
janioxavier <cloud_8945@hotmail.com>
2015-11-28 12:35:01 -0300

    Cabeçalho completo de BellmanFord.

--- BellmanFord.h
+++ BellmanFord.h
@@ -1,8 +1,27 @@
 #pragma once
+#include <list>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include "Edge.h"
 
-#include "AdjacencyList.h"
+#define INF INT_MAX/2
+#define null -1
 
 class BellmanFord {
-public:
-private:
+ public:
+  BellmanFord();
+  bool bellmanFord(std::list<Edge<int,int,int> > edgeList, 
+		   std::list<int> vertexList, int source);
+  std::vector<int> getDistance_() const;
+  std::vector<int> getPredecessor_() const;
+  std::string toString();
+  ~BellmanFord();
+ private:
+  void init(std::list<int> vertexList, int source);
+  void relaxEdges(std::list<Edge<int,int,int> > edgeList, int size);
+  bool hasNegativeCycle(std::list<Edge<int,int,int> > edgeList);
+  std::vector<int> distance_;
+  std::vector<int> predecessor_;
 };
